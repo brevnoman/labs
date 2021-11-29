0a9077923d5fd9dad79a8f635c0fdb3fd2713e9c
Sautin Dmytro <dimonsova1488@gmail.com>
2021-11-29 11:43:50 +0200

    done

--- calculator.py
+++ calculator.py
@@ -1,9 +1,31 @@
+import unittest
+
+
 class Calculator:
- def add(self, a, b):
-   return a+b
- def subtract(self, a, b):
-   return a-b
- def multiply(self, a, b):
-   return a*b
- def divide(self, a, b):
-   return a/b
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
+
+
+class CalculatorTest(unittest.TestCase):
+    a = 5
+    b = 6
+    def test_add(self):
+        result = Calculator.add(a=self.a, b=self.b)
+        self.assertEqual(result, 11)
+    def test_subtract(self):
+        result = Calculator.subtract(a=self.a, b=self.b)
+        self.assertEqual(result, -1)
+    def test_multiply(self):
+        result = Calculator.multiply(a=self.a, b=self.b)
+        self.assertEqual(result, 30)
+    def test_divide(self):
+        result = Calculator.divide(a=self.a, b=self.b)
--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def get_reply(number):
 	if number%5==0 and number%3==0:
 		return 'FizzBuzz'
@@ -7,3 +10,22 @@ def get_reply(number):
 		return 'Buzz'
 	else:
 		return ''
+
+
+class FizzBuzzTest(unittest.TestCase):
+	def test_fizz(self):
+		input_num = 3
+		result = get_reply(input_num)
+		self.assertEqual(result, "Fizz")
+	def test_buzz(self):
+		input_num = 5
+		result = get_reply(input_num)
+		self.assertEqual(result, "Buzz")
+	def test_fizzbuzz(self):
+		input_num = 15
+		result = get_reply(input_num)
+		self.assertEqual(result, "FizzBuzz")
+	def test_else(self):
+		input_num = 4
+		result = get_reply(input_num)
+		self.assertTrue(result == "")
