6752e11a88b60835308d37ce1363815f0660c61c
Sautin Dmytro <dimonsova1488@gmail.com>
2021-11-29 17:25:40 +0200

    remastered

--- calculator.py
+++ calculator.py
@@ -2,30 +2,39 @@ import unittest
 
 
 class Calculator:
-    def add(self, a, b):
+    @staticmethod
+    def add(a, b):
         return a + b
 
-    def subtract(self, a, b):
+    @staticmethod
+    def subtract(a, b):
         return a - b
 
-    def multiply(self, a, b):
+    @staticmethod
+    def multiply(a, b):
         return a * b
 
-    def divide(self, a, b):
+    @staticmethod
+    def divide(a, b):
         return a / b
 
 
 class CalculatorTest(unittest.TestCase):
     a = 5
     b = 6
+
     def test_add(self):
         result = Calculator.add(a=self.a, b=self.b)
         self.assertEqual(result, 11)
+
     def test_subtract(self):
         result = Calculator.subtract(a=self.a, b=self.b)
         self.assertEqual(result, -1)
+
     def test_multiply(self):
         result = Calculator.multiply(a=self.a, b=self.b)
         self.assertEqual(result, 30)
+
     def test_divide(self):
         result = Calculator.divide(a=self.a, b=self.b)
+        self.assertEqual(result, 5/6)
