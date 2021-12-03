1f952155e2801007318f63c02c792719c976285c
brevnoman <dimasautin8@gmail.com>
2021-12-03 16:46:19 +0200

    Some_things

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,19 +1,12 @@
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect, HttpResponseRedirect
-from django.views.generic import View, DetailView
+from django.views.generic import View
 from mainapp.models import Product, Category, Customer, Cart, CartProduct
 
 
-# def all_product(request):
-#     products_qs = Product.objects.all()
-#     return render(request, "all_products.html", context={"products": products_qs})
 class MainView(View):
     categories = Category.objects.all()
-
-    # def dispatch(self, request, *args, **kwargs):
-    #     if request.user.is_authenticated:
-    #         pass
 
     def get_cart(self, request):
         if request.user.is_authenticated:
@@ -46,10 +39,6 @@ class AllProducts(MainView):
                       {"cart": cart, "products": products_qs, "categories": self.categories})
 
 
-# def products(request, slug):
-#     products_qs = Product.objects.get(slug=slug)
-#     return render(request, "product_detail.html", context={"product": products_qs})
-
 class Products(MainView):
 
     def get(self, request, slug, *args, **kwargs):
@@ -59,30 +48,12 @@ class Products(MainView):
                       {"cart": cart, "product": products_qs, "categories": self.categories})
 
 
-# def main_page(request):
-#     products_qs = Product.objects.all()[:8]
-#     categories_qs = Category.objects.all()
-#     return render(request, "main_page.html", context={"categories": categories_qs, "products": products_qs})
-
 class MainPage(MainView):
     def get(self, request, *args, **kwargs):
         products_qs = Product.objects.all()[:8]
         cart = self.get_cart(request)
         return render(request, "main_page.html",
                       context={"cart": cart, "products": products_qs, "categories": self.categories})
-
-
-# def register_page(request):
-#     if request.user.is_authenticated:
-#         return redirect("/")
-#     if request.method == "POST":
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             return redirect("/")
-#     form = UserCreationForm()
-#     return render(request, "register.html", context={"form": form})
 
 
 class RegisterPage(MainView):
@@ -101,22 +72,6 @@ class RegisterPage(MainView):
             login(request, user)
             return redirect("/")
         return redirect("register_page")
-
-
-# def login_page(request):
-#     if request.user.is_authenticated:
-#         return redirect("/")
-#     if request.method == "POST":
-#         form = AuthenticationForm(request, data=request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data.get("username")
-#             password = form.cleaned_data.get("password")
-#             user = authenticate(username=username, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect("/")
-#     form = AuthenticationForm
-#     return render(request, "login.html", context={"form": form})
 
 
 class LoginPage(MainView):
@@ -139,12 +94,6 @@ class LoginPage(MainView):
         return redirect("login_page")
 
 
-# def category_products(request, slug):
-#     category = Category.objects.get(slug=slug)
-#     category_qs = Product.objects.filter(category=category)
-#     return render(request, "category_products.html", context={"products": category_qs, "slug": slug})
-
-
 class CategoryProducts(MainView):
     def get(self, request, slug, *args, **kwargs):
         cart = self.get_cart(request)
@@ -157,10 +106,6 @@ class CategoryProducts(MainView):
 def logout_user(request):
     logout(request)
     return redirect("main_page")
-
-
-# class AllProducts(MainView):
-#     def get(self, request, *args, **kwargs):
 
 
 class CartView(MainView):
